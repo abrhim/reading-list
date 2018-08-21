72b6b66d5e12e3d4d4a3ffb918120f7d6dada03a
Abram Himmer <abram@Abrams-MacBook-Pro.local>
2018-08-20 21:47:27 -1000

    Finished Product

---
+++ app/components/reading-list.js
@@ -0,0 +1,17 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    title: "",
+    actions:{
+        onEnter(){
+            const title = this.get('title');
+            if (title) {
+                this.get('addBook')({ title });
+            }
+            this.set('title',"");
+        }
+    }
+
+
+});
